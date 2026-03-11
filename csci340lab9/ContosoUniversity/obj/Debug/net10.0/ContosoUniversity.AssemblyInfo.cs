3ce8fd189b7b46b98d979d54b0fcc4376d468eb4
SamCT <colliertenisonsa@hendrix.edu>
2026-03-10 21:24:14 -0500

    LAb 9 pt 8

--- csci340lab9/ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ csci340lab9/ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6430566df8730c533b93ed1f8e2259c998158b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d11d5331fda461304c51258eb9556bed7670beca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
